86e949da7c4d5cc0b71626e95c8b063d0648ca4c
Andrii <andreivarlamov228@gmail.com>
2025-10-14 19:58:51 -0400

    Player, movement, shield, boundaries
    First edition

---
+++ SpaceShmup/Assets/_Scrips/Hero.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Hero : MonoBehaviour
+{
+   static public Hero S { get; private set; }  // Singleton property   
+ 
+    [Header("Inscribed")]
+   // These fields control the movement of the ship
+    public float speed = 50;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    [Header("Dynamic")][Range(0, 4)]                                          
+    public float shieldLevel = 1;    
+    void Awake()
+    {
+        if (S == null)
+        { 
+            S = this; 
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        Vector3 pos = transform.position;
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
+        transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+    }
+}
+
